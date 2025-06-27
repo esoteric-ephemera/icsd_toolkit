ce9d4ef838892648eabf93ea770bf8189b6b2604
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-06-27 15:47:33 -0700

    add enhanced cif to structure parsing

---
+++ icsd_toolkit_analysis/src/icsd_toolkit/analysis/schemas.py
@@ -0,0 +1,193 @@
+from __future__ import annotations
+
+from contextlib import redirect_stderr, redirect_stdout
+from io import StringIO
+import json
+from typing import TYPE_CHECKING
+import warnings
+
+from pathlib import Path
+from pydantic import BaseModel, field_serializer
+
+from pymatgen.core import Composition, Structure
+from pymatgen.io.cif import CifParser
+
+from icsd_toolkit.client.enums import IcsdSubset
+from icsd_toolkit.client.schemas import IcsdPropertyDoc
+
+try:
+    from pycodcif import parse as cod_cif_parse
+
+except ImportError:
+    cod_cif_parse = None
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+# These are only used for the pycodcif -> pymatgen interface
+if cod_cif_parse:
+    from tempfile import NamedTemporaryFile
+    from pymatgen.io.cif import CifBlock
+    from string import printable
+
+    ASCII_CHARS = set(printable)
+
+
+def get_chemsys_from_structure(structure: Structure):
+    return sorted(
+        set([element for element in structure.composition.remove_charges().as_dict()])
+    )
+
+
+def _pycodcif_to_pymatgen(
+    cif_str: str, cif_parser: CifParser | None = None
+) -> list[Structure]:
+
+    structures = []
+    if cod_cif_parse is None:
+        raise ImportError("Please pip install `pycodcif` to use this feature.")
+
+    temp_file = NamedTemporaryFile(suffix=".cif")
+    with open(temp_file.name, "w", encoding="utf-8") as f:
+        # remove non-ASCII characters
+        f.write("".join(filter(lambda x: x in ASCII_CHARS, cif_str)))
+        f.seek(0)
+
+    meta, num_errors, errors = cod_cif_parse(temp_file.name)
+    if num_errors:
+        warnings.warn("pycodcif:\n" + "\n  ".join(errors))
+
+    cif_parser = cif_parser or CifParser(temp_file.name)
+
+    for block in meta:
+        for k in ("data", "values"):
+            if (data := block.get(k, None)) is not None:
+                break
+        cif_block = CifBlock(data, *[block.get(k) for k in ("loops", "name")])
+        structures.append(
+            cif_parser._get_structure(cif_block, primitive=True, symmetrized=False)
+        )
+
+    temp_file.close()
+
+    return structures
+
+
+class IcsdStructureDoc(BaseModel):
+
+    chemsys: str | None = None
+    ions: list[str] | None = None
+    nelements: int | None = None
+    volume_per_atom: float | None = None
+    density: float | None = None
+    num_sites: int | None = None
+    space_group_number: int | None = None
+    space_group_symbol: str | None = None
+    is_ordered: bool | None = None
+    path: Path | None = None
+    icsd_id: int | None = None
+    remarks: list[str] | None = None
+    icsd_category: IcsdSubset | None = None
+    matched_icsd_ids: list[str] | None = None
+
+    structure: Structure | None = None
+    composition: dict[str, float] | None = None
+    cif: str | None = None
+
+    @field_serializer("structure")
+    def sanitize_structure_for_parquet(self, structure: Structure | None) -> str | None:
+        if structure is not None:
+            return json.dumps(structure.as_dict())
+        return None
+
+    @classmethod
+    def from_cif_str(cls, cif_str: str, **kwargs) -> Self:
+
+        config = {
+            "remarks": [],
+            **kwargs,
+        }
+        with StringIO(initial_value=cif_str) as stream:
+            parser = CifParser(stream)
+
+        parse_fail = False
+        try:
+            with redirect_stderr(StringIO()), redirect_stdout(StringIO()):
+                structure = parser.parse_structures(primitive=True)[0]
+
+        except Exception as exc_pmg:
+
+            config["remarks"].append(str(exc_pmg))
+            if cod_cif_parse:
+                try:
+
+                    structure = _pycodcif_to_pymatgen(cif_str, cif_parser=parser)[0]
+                except Exception as exc_pycodcif:
+                    config["remarks"].append(str(exc_pycodcif))
+                    parse_fail = True
+            else:
+                parse_fail = True
+
+        if parse_fail:
+            config["cif"] = cif_str
+            return cls(**config)
+
+        composition = Composition(structure.composition.as_dict())
+
+        # Some ICSD structures are disordered only in manually assigned oxidation states
+        if struct_is_disordered := not structure.is_ordered:
+
+            try:
+                if (
+                    Structure(
+                        structure.lattice,
+                        species=[site.species for site in structure],
+                        coords=structure.frac_coords,
+                        coords_are_cartesian=False,
+                        charge=structure.charge,
+                    )
+                    .remove_oxidation_states()
+                    .is_ordered
+                ):
+                    struct_is_disordered = False
+
+            except ValueError:
+                pass
+
+        config.update(
+            {
+                "structure": structure,
+                "ions": [str(ele) for ele in structure.composition],
+                "chemsys": "-".join(get_chemsys_from_structure(structure)),
+                "num_elements": len(composition.elements),
+                "volume_per_atom": structure.volume / structure.num_sites,
+                "density": structure.density,
+                "is_ordered": struct_is_disordered,
+            }
+        )
+
+        with redirect_stderr(StringIO()), redirect_stdout(StringIO()):
+            remarks = parser.check(structure)
+        if remarks:
+            config["remarks"].append(remarks)
+
+        try:
+            config["space_group_symbol"], config["space_group_number"] = (
+                structure.get_space_group_info()
+            )
+        except Exception as exc:
+            config["remarks"].append(str(exc))
+
+        if len(config["remarks"]) == 0:
+            config["remarks"] = None
+
+        return cls(**config)
+
+    @classmethod
+    def from_icsd_property_doc(cls, doc: IcsdPropertyDoc) -> Self:
+        if doc.cif is None:
+            return cls(icsd_id=doc.collection_code)
+        return cls.from_cif_str(
+            doc.cif,
+            icsd_id=doc.collection_code,
+        )
--- icsd_toolkit_client/src/icsd_toolkit/client/client.py
+++ icsd_toolkit_client/src/icsd_toolkit/client/client.py
@@ -6,24 +6,30 @@ This module is based on
 
 from __future__ import annotations
 
+import multiprocessing.queues
 import os
 import re
 import requests
 from time import time
 
+import logging
+
+import multiprocessing
 import numpy as np
 from pydantic import BaseModel, Field, PrivateAttr
 
 from icsd_toolkit.client.settings import IcsdClientSettings
-from icsd_toolkit.client.utils import (
+from icsd_toolkit.client.enums import (
     IcsdAdvancedSearchKeys,
     IcsdSubset,
     IcsdDataFields,
-    IcsdPropertyDoc,
 )
+from icsd_toolkit.client.schemas import IcsdPropertyDoc
 
 SETTINGS = IcsdClientSettings()
 _ICSD_TOKEN_TIMEOUT = 3600  # ICSD tokens expire in one hour
+
+logger = logging.getLogger("icsd_toolkit.client")
 
 
 class IcsdClient(BaseModel):
@@ -37,6 +43,7 @@ class IcsdClient(BaseModel):
     max_batch_size: float | None = Field(SETTINGS.MAX_BATCH_SIZE)
 
     use_document_model: bool = Field(True)
+    num_parallel_requests: int | None = Field(None)
 
     _auth_token: str | None = PrivateAttr(None)
     _session_start_time: float | None = PrivateAttr(None)
@@ -45,18 +52,20 @@ class IcsdClient(BaseModel):
     def _is_windows(self) -> bool:
         return os.name == "nt"
 
-    def refresh_session(self) -> None:
+    def refresh_session(self, force: bool = False) -> None:
         if self._session_start_time is None:
             self._session_start_time = time()
 
-        if self._auth_token is None or (
-            (time() - self._session_start_time) > 0.98 * _ICSD_TOKEN_TIMEOUT
+        if (
+            self._auth_token is None
+            or ((time() - self._session_start_time) > 0.98 * _ICSD_TOKEN_TIMEOUT)
+            or force
         ):
+            self.logout()
             self._session_start_time = time()
-            self.__enter__()
+            self.login()
 
-    def __enter__(self) -> None:
-
+    def login(self) -> None:
         response = requests.post(
             "https://icsd.fiz-karlsruhe.de/ws/auth/login",
             headers={
@@ -70,9 +79,12 @@ class IcsdClient(BaseModel):
         )
         if response.status_code == 200:
             self._auth_token = response.headers["ICSD-Auth-Token"]
+            if self._auth_token is None:
+                logger.info(response.content)
+        else:
+            logger.info(response.content)
 
-    def __exit__(self, *args) -> None:
-
+    def logout(self) -> None:
         _ = self._get(
             "https://icsd.fiz-karlsruhe.de/ws/auth/logout",
             headers={
@@ -82,8 +94,15 @@ class IcsdClient(BaseModel):
         self._auth_token = None
         self._session_start_time = None
 
+    def __enter__(self) -> None:
+        self.login()
+        return self
+
+    def __exit__(self, *args) -> None:
+        self.logout()
+
     def __del__(self) -> None:
-        self.__exit__()
+        self.logout()
 
     def _get(self, *args, **kwargs) -> requests.Response:
         self.refresh_session()
@@ -95,6 +114,8 @@ class IcsdClient(BaseModel):
         params = tuple(list(params) + [("windowsclient", self._is_windows)])
 
         resp = requests.get(*args, **kwargs, headers=headers, params=params)
+        if resp.status_code != 200:
+            logger.info(resp.content)
         return resp
 
     def _get_cifs(self, collection_codes: int | list[int]) -> dict[int, str]:
@@ -125,8 +146,11 @@ class IcsdClient(BaseModel):
         indices: list[int],
         properties: list[str | IcsdDataFields] | None = None,
         include_cif: bool = False,
+        include_metadata: bool = True,
+        _data: list | None = None,
     ) -> list:
 
+        self.refresh_session(force=True)
         search_props = [
             (
                 IcsdDataFields[prop].name
@@ -137,43 +161,68 @@ class IcsdClient(BaseModel):
         ]
 
         if len(indices) > self.max_batch_size:
-            batched_ids = np.array_split(
-                indices, np.ceil(len(indices) / self.max_batch_size)
-            )
+            batched_ids: list[list[str]] = [
+                v.tolist()
+                for v in np.array_split(
+                    indices, np.ceil(len(indices) / self.max_batch_size)
+                )
+            ]
 
             data = []
             for i, batch in enumerate(batched_ids):
-                data.extend(self._search(batch, properties=search_props))
+                data.extend(
+                    self._search(
+                        batch,
+                        properties=search_props,
+                        include_cif=include_cif,
+                        include_metadata=include_metadata,
+                        _data=_data,
+                    )
+                )
             return data
 
-        response = self._get(
-            "https://icsd.fiz-karlsruhe.de/ws/csv",
-            headers={
-                "accept": "application/csv",
-            },
-            params=(
-                ("idnum", tuple(indices)),
-                ("listSelection", search_props),
-            ),
-        )
+        if not include_cif and not include_metadata:
+            return list(indices)
 
-        data = []
-        if response.status_code == 200:
-            _data = [row.split("\t") for row in response.content.decode().splitlines()]
-            columns = _data[0][:-1]
+        if include_metadata:
+            response = self._get(
+                "https://icsd.fiz-karlsruhe.de/ws/csv",
+                headers={
+                    "accept": "application/csv",
+                },
+                params=(
+                    ("idnum", tuple(indices)),
+                    ("listSelection", search_props),
+                ),
+            )
 
-            data += [
-                {IcsdDataFields[k].value: row[i] for i, k in enumerate(columns)}
-                for row in _data[1:]
-            ]
+            data = []
+            if response.status_code == 200:
+                csv_data = [
+                    row.split("\t") for row in response.content.decode().splitlines()
+                ]
+                columns = csv_data[0][:-1]
+
+                data += [
+                    {IcsdDataFields[k].value: row[i] for i, k in enumerate(columns)}
+                    for row in _data[1:]
+                ]
+            else:
+                logger.info(response.content)
 
         if include_cif:
             cifs = self._get_cifs(indices)
-            for i, doc in enumerate(data):
-                data[i]["cif"] = cifs.get(int(doc["collection_code"]))
+            if include_metadata:
+                for i, doc in enumerate(data):
+                    data[i]["cif"] = cifs.get(int(doc["collection_code"]))
+            else:
+                data = [{"collection_code": cc, "cif": cif} for cc, cif in cifs.items()]
 
         if self.use_document_model:
-            return [IcsdPropertyDoc(**props) for props in data]
+            data = [IcsdPropertyDoc(**props) for props in data]
+
+        if _data:
+            _data.extend(data)
         return data
 
     def search(
@@ -181,6 +230,7 @@ class IcsdClient(BaseModel):
         subset: IcsdSubset | str | None = None,
         properties: list[str | IcsdDataFields] | None = None,
         include_cif: bool = False,
+        include_metadata: bool = True,
         **kwargs,
     ) -> list:
 
@@ -188,26 +238,55 @@ class IcsdClient(BaseModel):
         for k in IcsdAdvancedSearchKeys:
             if (v := kwargs.get(k.value)) is not None:
                 if isinstance(v, tuple):
-                    v = "-".join(v)
+                    v = f"{v[0]}-{v[1]}"
                 elif isinstance(v, list):
                     v = ",".join(v)
                 query_vars.append(f"{k.name.lower()} : {v}")
         query_str = " and ".join(query_vars)
 
-        self.refresh_session()
+        params = [("query", query_str)]
+        if subset:
+            params.append(("content type", IcsdSubset(subset).name))
+
         response = self._get(
             "https://icsd.fiz-karlsruhe.de/ws/search/expert",
             headers={
                 "accept": "application/xml",
             },
-            params=(
-                ("query", query_str),
-                ("content type", IcsdSubset(subset).name if subset else None),
-            ),
+            params=params,
         )
 
-        idxs = []
+        idxs: list[str] = []
         if matches := re.match(".*<idnums>(.*)</idnums>.*", response.content.decode()):
             idxs.extend(list(matches.groups())[0].split())
 
-        return self._search(idxs, properties=properties, include_cif=include_cif)
+        if self.num_parallel_requests and len(idxs) > self.num_parallel_requests:
+            batched_idxs = np.array_split(idxs, self.num_parallel_requests)
+
+            manager = multiprocessing.Manager()
+            procs = []
+            res = manager.list()
+            for iproc in range(self.num_parallel_requests):
+                proc = multiprocessing.Process(
+                    target=self._search,
+                    args=(batched_idxs[iproc].tolist(),),
+                    kwargs={
+                        "properties": properties,
+                        "include_cif": include_cif,
+                        "include_metadata": include_metadata,
+                        "_data": res,
+                    },
+                )
+                proc.start()
+                procs.append(proc)
+
+            for proc in procs:
+                proc.join()
+            return list(res)
+
+        return self._search(
+            idxs,
+            properties=properties,
+            include_cif=include_cif,
+            include_metadata=include_metadata,
+        )
