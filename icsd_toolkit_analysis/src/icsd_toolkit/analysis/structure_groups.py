2f957ba27f43f428249fa6f749edc839c6a623eb
esoteric-ephemera <aaron.kaplan.physics@gmail.com>
2025-07-02 14:48:21 -0700

    log aggregation

--- icsd_toolkit_analysis/src/icsd_toolkit/analysis/structure_groups.py
+++ icsd_toolkit_analysis/src/icsd_toolkit/analysis/structure_groups.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 
 from pathlib import Path
@@ -15,18 +14,28 @@ from icsd_toolkit.analysis.schemas import IcsdStructureDoc
 if TYPE_CHECKING:
     from pymatgen.core import Structure
 
+
+def aggregate_logs(cache_dir: str | Path) -> dict[str, list[list[int]]]:
+    processed_chemsys = {}
+    for p in Path(cache_dir).glob("*jsonl"):
+        with open(p, "r") as f:
+            for line in f:
+                processed_chemsys.update(json.loads(line))
+    return processed_chemsys
+
+
 class StructureGrouper:
 
-    __slots__ = ("structure_matcher","max_structure_size","nproc","cache_dir")
+    __slots__ = ("structure_matcher", "max_structure_size", "nproc", "cache_dir")
 
     def __init__(
         self,
-        structure_matcher : StructureMatcher | None = None,
-        max_structure_size : int | None = 100,
-        nproc : int = 1,
-        cache_dir : str | Path | None = None
+        structure_matcher: StructureMatcher | None = None,
+        max_structure_size: int | None = 100,
+        nproc: int = 1,
+        cache_dir: str | Path | None = None,
     ) -> None:
-        
+
         self.structure_matcher = structure_matcher or StructureMatcher(
             primitive_cell=True,
             attempt_supercell=True,
@@ -39,44 +48,41 @@ class StructureGrouper:
         if cache_dir:
             self.cache_dir = Path(cache_dir)
             if not self.cache_dir.exists():
-                self.cache_dir.mkdir(exist_ok=True,parents=True)
+                self.cache_dir.mkdir(exist_ok=True, parents=True)
 
     def _group_structures(
         self,
-        structures : dict[str, list[Structure]],
-        structure_groups : list[int],
-        cache_file : str | Path | None = None
+        structures: dict[str, list[Structure]],
+        structure_groups: list[int],
+        cache_file: str | Path | None = None,
     ) -> None:
-                    
+
         for cs, struct_in_cs in structures.items():
             groups = self.structure_matcher.group_structures(struct_in_cs)
             icsd_groups = [
-                sorted([structure._icsd_id for structure in group])
-                for group in groups
+                sorted([structure._icsd_id for structure in group]) for group in groups
             ]
-            
+
             structure_groups.extend(icsd_groups)
 
             if cache_file:
-                with open(cache_file,"a") as f:
+                with open(cache_file, "a") as f:
                     f.write(json.dumps({cs: icsd_groups}) + "\n")
 
     def group_structures(
         self,
-        structure_docs : list[IcsdStructureDoc],
+        structure_docs: list[IcsdStructureDoc],
     ) -> list[IcsdStructureDoc]:
 
         chemsys = set([doc.chemsys for doc in structure_docs])
-        by_chemsys = {
-            cs : [] for cs in chemsys
-        }
+        by_chemsys = {cs: [] for cs in chemsys}
 
-        unique_idx : list[int] = []
+        unique_idx: list[int] = []
         for doc in structure_docs:
             if self.max_structure_size and doc.num_sites > self.max_structure_size:
                 unique_idx.append([doc.icsd_id])
                 continue
-            
+
             struct = doc.structure
             struct._icsd_id = doc.icsd_id
             by_chemsys[doc.chemsys].append(struct)
@@ -85,8 +91,8 @@ class StructureGrouper:
             if len(by_chemsys[cs]) == 1:
                 struct = by_chemsys.pop(cs)[0]
                 unique_idx.append([struct._icsd_id])
-            
-        sorted_chemsys = sorted(by_chemsys,key = lambda k : len(by_chemsys[k]))
+
+        sorted_chemsys = sorted(by_chemsys, key=lambda k: len(by_chemsys[k]))
         process_targets = [{} for _ in range(self.nproc)]
         for iproc, cs in enumerate(sorted_chemsys):
             process_targets[iproc % self.nproc][cs] = by_chemsys[cs]
@@ -103,13 +109,13 @@ class StructureGrouper:
 
             proc = multiprocessing.Process(
                 target=self._group_structures,
-                args = (
+                args=(
                     process_targets[iproc],
                     grouped_ids,
                 ),
                 kwargs=proc_kwargs,
             )
-            
+
             proc.start()
             procs.append(proc)
 
@@ -117,9 +123,7 @@ class StructureGrouper:
             proc.join()
 
         unique_docs = []
-        docs_by_icsd_id = {
-            doc.icsd_id: doc for doc in structure_docs
-        }
+        docs_by_icsd_id = {doc.icsd_id: doc for doc in structure_docs}
         for id_group in list(grouped_ids):
             min_idx = id_group[0]
             doc = docs_by_icsd_id[min_idx].model_dump()
@@ -127,8 +131,10 @@ class StructureGrouper:
             unique_docs.append(IcsdStructureDoc(**doc))
 
         return unique_docs
-    
-    def group_structures_from_dataframe(self, dataframe : pd.DataFrame, **kwargs) -> list[IcsdStructureDoc]:
+
+    def group_structures_from_dataframe(
+        self, dataframe: pd.DataFrame, **kwargs
+    ) -> list[IcsdStructureDoc]:
 
         icsd_docs = []
         for idx in dataframe.index:
@@ -137,5 +143,5 @@ class StructureGrouper:
                 icsd_docs.append(doc)
             except Exception:
                 continue
-        
+
         return self.group_structures(icsd_docs, **kwargs)
